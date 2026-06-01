7dc4de433783a3ec5b3068424e07c356f3e3f7d1
Philipp Bischof <Bischof@sitepark.com>
2026-06-01 08:00:40 +0200

    feat: Errorhandling for threshold and StepExecution

---
+++ src/Exception/StepExecution.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Atoolo\Crawler\Exception;
+
+class StepExecution extends \Exception
+{
+
+    public function __construct(string $stepName, string $message, \Throwable $previous)
+    {
+        parent::__construct(
+            "Step [$stepName] failed: $message",
+            0,
+            $previous
+        );
+    }
+}
