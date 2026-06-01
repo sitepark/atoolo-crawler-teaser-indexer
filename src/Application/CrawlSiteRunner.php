460092b24db26d18b7a932f37a17e773423d8250
Philipp Bischof <Bischof@sitepark.com>
2026-06-01 08:29:30 +0200

    refactor: Errorhandling if sp_id is missing

--- src/Application/CrawlSiteRunner.php
+++ src/Application/CrawlSiteRunner.php
@@ -6,6 +6,7 @@ namespace Atoolo\Crawler\Application;
 
 use Atoolo\Crawler\Config\CrawlerConfigContext;
 use Atoolo\Crawler\Controller\CrawlerManager;
+use Atoolo\Crawler\Exception\SiteIdExecution;
 use Psr\Log\LoggerInterface;
 
 final class CrawlSiteRunner
@@ -14,8 +15,7 @@ final class CrawlSiteRunner
         private readonly CrawlerConfigContext $configContext,
         private readonly CrawlerManager $crawlerManager,
         private readonly LoggerInterface $logger,
-    ) {
-    }
+    ) {}
     /**
      * @param array<string, mixed> $site
      */
@@ -24,6 +24,13 @@ final class CrawlSiteRunner
         $this->configContext->set($site);
         /** @var string $siteKey */
         $siteKey = $site['sp_id'] ?? null;
+
+        if (!is_string($siteKey) || $siteKey === '') {
+            $this->logger->error('Invalid site config: missing "sp_id" field.');
+            throw new \InvalidArgumentException(
+                'Site config is missing required field "sp_id".'
+            );
+        }
 
         try {
             $this->logger->info(sprintf('Processing site: %s', $siteKey));
