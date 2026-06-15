e4b8e83eccb946d9569be752b691dd3f52d4a038
Philipp Bischof <Bischof@sitepark.com>
2026-06-15 14:23:13 +0200

    test: Test the ConsoleApplication

---
+++ tests/ConsoleApplicationTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use Atoolo\Crawler\Console\Application;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Application as BaseApplication;
+use Symfony\Component\Console\CommandLoader\CommandLoaderInterface;
+
+final class ConsoleApplicationTest extends TestCase
+{
+    public function testApplicationIsInstanceOfBaseApplication(): void
+    {
+        $loader = $this->createStub(CommandLoaderInterface::class);
+        $app    = new Application($loader);
+
+        $this->assertInstanceOf(BaseApplication::class, $app);
+    }
+}
