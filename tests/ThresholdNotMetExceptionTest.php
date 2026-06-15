bacae640dabab450643e5cb323de2a723c42ebee
Philipp Bischof <Bischof@sitepark.com>
2026-06-15 15:20:55 +0200

    test: Tests ensures that the treshold is working as aspected

---
+++ tests/ThresholdNotMetExceptionTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use Atoolo\Crawler\Exception\ThresholdNotMetException;
+use PHPUnit\Framework\TestCase;
+
+final class ThresholdNotMetExceptionTest extends TestCase
+{
+    public function testExceptionMessageContainsCountsAndThreshold(): void
+    {
+        $exception = new ThresholdNotMetException(3, 10);
+
+        $this->assertStringContainsString('3', $exception->getMessage());
+        $this->assertStringContainsString('10', $exception->getMessage());
+    }
+
+    public function testExceptionIsInstanceOfException(): void
+    {
+        $exception = new ThresholdNotMetException(0, 5);
+
+        $this->assertInstanceOf(\Exception::class, $exception);
+    }
+}
